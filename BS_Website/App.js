4cd2d4ff61855eccf359b678b588cb64d81f43bc
Salih Saygi <salih.saygie@gmail.com>
2021-10-13 12:39:03 -0700

    Changes for Operations Department

--- BS_Website/App.js
+++ BS_Website/App.js
@@ -1,142 +1,328 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, Linking, SafeAreaView, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Linking,
+  // SafeAreaView, //never used
+  // FlatList, //never used
+} from 'react-native';
 import { Button, Divider } from 'react-native-elements';
-import logo from "./assets/logo.png";
+import logo from './assets/logo.png';
 import List from './List.js';
 import Desc from './Desc.js';
-import OppDesc from './OppDesc.js';
+// import OppDesc from './OppDesc.js'; //did not need it
 import Sam from './assets/sam_am.jpeg';
-
+import Kinza from './assets/Kinza_Tauseef.jfif';
+import Angie from './assets/Angei_Nechikat.jpg';
+import Valerie from './assets/Valerie_Okafor.jfif';
+import Melanie from './assets/Melanie_Baeza.jpeg';
 
 export default function Marketing() {
-
   return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1}}>
-        <Image source={logo} style = {{height: 150, width:350, alignSelf: 'center'}} />
-        <Text style = {styles.header}>MARKETING DEPARTMENT</Text>
-        <Text style={styles.DepDesc}>
-            Marketing is the creative department of Business Society! Marketing is responsible for the promotion and reputation of the Society. We are currently focusing on growing our social media through audience engagement and virtual promotion.
-        </Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Image
+        source={logo}
+        style={{ height: 150, width: 350, alignSelf: 'center' }}
+      />
+      <Text style={styles.header}>OPERATIONS DEPARTMENT</Text>
+      <Text style={styles.DepDesc}>
+        This department shall be led by the Chief Operations Officer. The
+        objective of this department is to plan, coordinate, and execute
+        successful events. This department is responsible for coordinating
+        Departments and resources outside of the Society for the execution of
+        events of the Society.
+      </Text>
 
-        {/*<Text style= {styles.header}> *Rotating Images Place Holder*</Text>*/}
+      {/*<Text style= {styles.header}> *Rotating Images Place Holder*</Text>*/}
 
-        <View style={styles.subBox}>
-            <Text style= {styles.subheader}> Upcoming Events </Text>
-        </View>
-        <List />
+      <View style={styles.subBox}>
+        <Text style={styles.subheader}> Upcoming Events </Text>
+      </View>
+      <List />
 
-        <View style={styles.subBox}>
-            <Text style= {styles.subheader}> Forms and Surveys </Text>
+      <View style={styles.subBox}>
+        <Text style={styles.subheader}> Forms and Surveys </Text>
+      </View>
+      <View style={styles.test}>
+        <View style={{ flex: 1 }}>
+          <Text style={{ padding: 10, textAlign: 'center' }}>
+            Haunted House 2021 Survey
+          </Text>
         </View>
-        <View style={styles.test}>
-          <View style={{ flex: 1 }}>
-              <Text style={{padding:10, textAlign: 'center'}}>Form 1</Text>
-          </View>
-          <View style={{ flex: 1}}>
-            <Button title = "Link" onPress={()=> alert("Link Unavaliable")}/>
-          </View>
+        <View style={{ flex: 1 }}>
+          <Button
+            title="Link"
+            onPress={() => {
+              Linking.openURL('https://forms.gle/3hVu1WuSsRYSwgp39');
+            }}
+          />
         </View>
-        <View style={styles.test}>
-          <View style={{ flex: 1 }}>
-              <Text style={{padding:10, textAlign: 'center'}}>Survey</Text>
-          </View>
-          <View style={{ flex: 1}}>
-            <Button title = "Link" onPress={()=> alert("Link Unavaliable")}/>
-          </View>
+      </View>
+      <View style={styles.test}>
+        <View style={{ flex: 1 }}>
+          <Text style={{ padding: 10, textAlign: 'center' }}>
+            Photo Booth Form
+          </Text>
         </View>
+        <View style={{ flex: 1 }}>
+          <Button title="Link" onPress={() => alert('Link Unavaliable')} />
+        </View>
+      </View>
 
-        <View style={styles.subBox}>
-            <Text style= {styles.subheader}> Opportunities </Text>
-        </View>
+      <View style={styles.subBox}>
+        <Text style={styles.subheader}> Opportunities </Text>
+      </View>
 
-        <OppDesc 
-          name='Internship'
-          desc = 'Coming Soon Fall 2021'
-          linkAv = {false}
-          link = ''
+      <Text>COMING SOON!</Text>
+
+      <View style={styles.subBox}>
+        <Text style={styles.subheader}> Resources </Text>
+      </View>
+
+      <Text>COMING SOON!</Text>
+
+      <View style={styles.subBox}>
+        <Text style={styles.subheader}> Department Members </Text>
+      </View>
+
+      <Text style={styles.memberNames}>Jianna San Gabriel</Text>
+      <Divider
+        style={{
+          backgroundColor: 'black',
+          height: 1.5,
+          marginBottom: 10,
+          maxWidth: 150,
+          marginLeft: 138,
+        }}
+      />
+
+      {/*uncomment and place imported image in the 'source' attribute below*/}
+      <Image
+        source={Sam}
+        style={{
+          width: 305,
+          height: 320,
+          alignSelf: 'center',
+          marginBottom: 5,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}
+      />
+
+      <View style={styles.memberDescipritons}>
+        <Desc
+          position="Chief Operations Officer"
+          major="Management and Business Economics"
+          year="3rd Year, Junior"
+          hasMinor={false}
         />
-        
-
-        <View style={styles.subBox}>
-            <Text style= {styles.subheader}> Resources </Text>
-        </View>
-        <OppDesc 
-          name='School Resources'
-          desc = 'Coming Soon Fall 2021'
-          linkAv = {false}
-          link = ''
+        <Button
+          title="LinkedIn"
+          style={{ paddingTop: 5 }}
+          onPress={() =>
+            Linking.openURL(
+              'https://www.linkedin.com/in/jianna-san-gabriel-8a61281b0/'
+            )
+          }
         />
-        <OppDesc 
-          name='Buisness Society Peer Assistance'
-          desc = 'Coming Soon Fall 2021'
-          linkAv = {true}
-          link = 'sample link'
+      </View>
+      <Text style={styles.memberNames}>Angie Nechikat</Text>
+      <Divider
+        style={{
+          backgroundColor: 'black',
+          height: 1.5,
+          marginBottom: 10,
+          maxWidth: 175,
+          marginLeft: 125,
+        }}
+      />
+      {/*uncomment and place imported image in the 'source' attribute below*/}
+
+      <Image
+        source={Angie}
+        style={{
+          width: 305,
+          height: 320,
+          alignSelf: 'center',
+          marginBottom: 5,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}
+      />
+
+      <View style={styles.memberDescipritons}>
+        <Desc
+          position="Vice President of Operations"
+          major="Management and Business Economics"
+          year="2nd Year, Sophomore"
+          hasMinor={false}
         />
+        <Button
+          title="LinkedIn"
+          style={{ paddingTop: 5 }}
+          onPress={() => alert('https://www.linkedin.com/in/angel-nechikat/')}
+        >
+          LinkedIn
+        </Button>
+      </View>
+      <Text style={styles.memberNames}>Valerie Okafor</Text>
+      <Divider
+        style={{
+          backgroundColor: 'black',
+          height: 1.5,
+          marginBottom: 10,
+          maxWidth: 175,
+          marginLeft: 125,
+        }}
+      />
+      {/*uncomment and place imported image in the 'source' attribute below*/}
 
+      <Image
+        source={Valerie}
+        style={{
+          width: 305,
+          height: 320,
+          alignSelf: 'center',
+          marginBottom: 5,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}
+      />
 
-        <View style={styles.subBox}>
-            <Text style= {styles.subheader}> Department Members </Text>
-        </View>
+      <View style={styles.memberDescipritons}>
+        <Desc
+          position="Inventory Analyst"
+          major="Management and Business Economics"
+          year="3rd Year, Junior"
+          hasMinor={false}
+        />
+        <Button
+          title="LinkedIn"
+          style={{ paddingTop: 5 }}
+          onPress={() => alert('https://www.linkedin.com/in/valerieokafor/')}
+        >
+          LinkedIn
+        </Button>
+      </View>
+      <Text style={styles.memberNames}>Kinza Tauseef</Text>
+      <Divider
+        style={{
+          backgroundColor: 'black',
+          height: 1.5,
+          marginBottom: 10,
+          maxWidth: 175,
+          marginLeft: 125,
+        }}
+      />
+      {/*uncomment and place imported image in the 'source' attribute below*/}
 
-        <Text style = {styles.memberNames}>Samantha Ammari</Text>
-        <Divider style={{backgroundColor: 'black', height:1.5, marginBottom:10, maxWidth:150, marginLeft: 138}}/>
+      <Image
+        source={Kinza}
+        style={{
+          width: 305,
+          height: 320,
+          alignSelf: 'center',
+          marginBottom: 5,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}
+      />
 
-        {/*uncomment and place imported image in the 'source' attribute below*/}
-        <Image source = {Sam} style = {{width:305, height: 320, alignSelf: 'center', marginBottom:5, borderColor: 'black', borderWidth: 2}}/>
+      <View style={styles.memberDescipritons}>
+        <Desc
+          position="Society Event Planner"
+          major="Management and Business Economics"
+          year="3rd Year, Junior"
+          hasMinor={false}
+        />
+        <Button
+          title="LinkedIn"
+          style={{ paddingTop: 5 }}
+          onPress={() => alert('https://www.linkedin.com/in/kinza-tauseef/')}
+        >
+          LinkedIn
+        </Button>
+      </View>
+      <Text style={styles.memberNames}>Melanie Baeza</Text>
+      <Divider
+        style={{
+          backgroundColor: 'black',
+          height: 1.5,
+          marginBottom: 10,
+          maxWidth: 175,
+          marginLeft: 125,
+        }}
+      />
+      {/*uncomment and place imported image in the 'source' attribute below*/}
 
-        <View style={styles.memberDescipritons}>
-            <Desc 
-              position='Chief Marketing Officer' 
-              major='Management and Business Economics' 
-              year = '2nd Year, Sophomore'
-              hasMinor = {false}
-            />
-            <Button title = "LinkedIn" style={{paddingTop: 5}} onPress={()=> Linking.openURL('https://www.linkedin.com/in/samantha-ammari/')}/>
-        </View>
-        <Text style = {styles.memberNames}>Lesley</Text>
-        <Divider style={{backgroundColor: 'black', height:1.5, marginBottom:10, maxWidth:175, marginLeft: 125}}/>
-        {/*uncomment and place imported image in the 'source' attribute below*/}
-        {/*
-        <Image source = {vanessa} style = {{width:305, height: 320, alignSelf: 'center', marginBottom:5, borderColor: 'black', borderWidth: 2}}/>
-          */}
+      <Image
+        source={Melanie}
+        style={{
+          width: 305,
+          height: 320,
+          alignSelf: 'center',
+          marginBottom: 5,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}
+      />
 
-        <View style={styles.memberDescipritons}>
-            <Desc 
-              position='Marketing Department Chair' 
-              major='' 
-              minor='' 
-              year = ''
-            />
-            <Button title = "LinkedIn" style={{paddingTop: 5}} onPress={()=> alert("LinkedIn Unavaliable")}>LinkedIn</Button>
-        </View>
-        <Text style={{textAlign:'center', color:'#b0aea9', paddingBottom:5, paddingTop:5}}>Business Society @ UCM</Text>
-        <StatusBar style="auto" />
-      </ScrollView>
+      <View style={styles.memberDescipritons}>
+        <Desc
+          position="Itinerary Analyst"
+          major="Management and Business Economics"
+          year="4th Year, Senior"
+          hasMinor={false}
+        />
+        <Button
+          title="LinkedIn"
+          style={{ paddingTop: 5 }}
+          onPress={() => alert('https://www.linkedin.com/in/angel-nechikat/')}
+        >
+          LinkedIn
+        </Button>
+      </View>
+      <Text
+        style={{
+          textAlign: 'center',
+          color: '#b0aea9',
+          paddingBottom: 5,
+          paddingTop: 5,
+        }}
+      >
+        Business Society @ UCM
+      </Text>
+      <StatusBar style="auto" />
+    </ScrollView>
   );
 }
 
-const Bold = ({ children }) => <Text style={{ fontWeight: 'bold' }}>{children}</Text>
-
+const Bold = ({ children }) => (
+  <Text style={{ fontWeight: 'bold' }}>{children}</Text>
+);
 
 const styles = StyleSheet.create({
-  test:{
+  test: {
     flexDirection: 'row',
     padding: 10,
     textAlign: 'center',
   },
-  subheader:{
+  subheader: {
     color: '#800000',
     fontSize: 25,
     textAlign: 'center',
     padding: 10,
   },
-  subBox:{
+  subBox: {
     textAlign: 'center',
     borderBottomColor: 'black',
     borderBottomWidth: 1.5,
     marginTop: 15,
-    marginBottom:10,
+    marginBottom: 10,
   },
   header: {
     color: '#800000',
@@ -145,22 +331,22 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   DepDesc: {
-    color: 'white', 
-    fontSize:15, 
-    paddingBottom:20, 
-    paddingTop:10,
+    color: 'white',
+    fontSize: 15,
+    paddingBottom: 20,
+    paddingTop: 10,
     textAlign: 'center',
     backgroundColor: '#ad0707',
     borderColor: 'black',
     borderRadius: 0,
-    borderWidth: 3
+    borderWidth: 3,
   },
   memberNames: {
-    fontSize:20,
+    fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
     paddingTop: 30,
-    borderBottomWidth:2,
+    borderBottomWidth: 2,
     borderBottomColor: 'black',
   },
   memberDescipritons: {
--- BS_Website/List.js
+++ BS_Website/List.js
@@ -1,71 +1,51 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
-
 class List extends Component {
-    state = {
-        events: [
-           {
-              id: 0,
-              name: 'Mixer',
-              date: '09/13/21',
-              desc: 'Place holder event description',
-           },
-           {
-              id: 1,
-              name: 'Guest Speaker',
-              date: '09/29/21',
-              desc: '',
-           },
-           {
-              id: 2,
-              name: '1st General Meeting',
-              date: '10/15/21',
-              desc: '',
-           },
-           {
-              id: 3,
-              name: 'Game Night',
-              date: '11/03/21',
-              desc: '',
-           }
-        ]
-    }
+  state = {
+    events: [
+      {
+        id: 0,
+        name: 'Annual Hounted House',
+        date: '10/28/21 -10/29/21',
+        desc: '@ The Den',
+      },
+    ],
+  };
 
-    displayDesc = (item) => {
-        alert(item.desc)
-    }
+  displayDesc = item => {
+    alert(item.desc);
+  };
 
-     render() {
-        return (
-           <View>
-              {
-                 this.state.events.map((item, index) => (
-                    <TouchableOpacity
-                       key = {item.id}
-                       style = {styles.container}
-                       onPress = {() => this.displayDesc(item)}>
-                       <Text style = {styles.text}>
-                          {item.name}     {item.date}
-                       </Text>
-                    </TouchableOpacity>
-                 ))
-              }
-           </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        {this.state.events.map((item, index) => (
+          <TouchableOpacity
+            key={item.id}
+            style={styles.container}
+            onPress={() => this.displayDesc(item)}
+          >
+            <Text style={styles.text}>
+              {item.name} {item.date}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  }
 }
 
-export default List
-  
-  const styles = StyleSheet.create ({
-     container: {
-        padding: 15,
-        marginTop: 5,
-        backgroundColor: '#f2dc18',
-        alignItems: 'center',
-     },
-     text: {
-        color: '#4f603c'
-     },
+export default List;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    marginTop: 5,
+    backgroundColor: '#f2dc18',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#4f603c',
+  },
 });
